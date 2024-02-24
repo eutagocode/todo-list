6f7ca17b53744402860399b69751969e63baad9b
eutagocode <thiagoluix21@gmail.com>
2024-02-24 17:41:09 -0300

    feat: :sparkles: Foi criada a função para o navegador ler as tarefa e colocá-las no documento dentro da tag com a classe tasks ao criar a tarefa

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,47 @@ const addTaskInput = body.querySelector("#addTaskInput");
 const addTaskButton = body.querySelector("#addTaskButton");
 const tasksContainer = body.querySelector("#tasks");
 
+window.addEventListener("DOMContentLoaded", readTasks);
+
 // Modelo da tarefa
 class TaskModel {
     constructor(text) {
-        this.id = new Date().getTime();
+        this.id = JSON.stringify(new Date().getTime());
         this.text = text;
         this.done = false;
+    }
+}
+
+// Função para ler as tarefas
+function readTasks() {
+    const tasks = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE) || "[]");
+    let taskElement = "";
+
+    // Lendo o array de tarefas e criando o modelo delas
+    tasks.map((item) => {
+        let task = `
+        <li class="task" id="${item.id}">
+            <p>${item.text}</p>
+            <div class="manage-task">
+                <button class="done">
+                    <img src="./assets/docs/images/check.svg" alt="Ícone de visto">
+                </button>
+                <button class="trash">
+                    <img src="./assets/docs/images/trash.svg" alt="Ícone de lixeira">
+                </button>
+            </div>
+        </li>
+        `;
+
+        taskElement += task;
+    });
+
+    tasksContainer.innerHTML = taskElement;
+
+    if (tasksContainer.innerHTML == "") {
+        tasksContainer.style = "display: none";
+    } else {
+        tasksContainer.style = "display: flex";
     }
 }
 
@@ -29,6 +64,8 @@ function addTasks() {
     localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(tasks));
 
     addTaskInput.value = "";
+
+    readTasks();
 }
 
 // Passando a função addTasks para o evento de click do botão
